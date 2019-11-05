e1dd6dcf6524270d0274cbdcbdc5a42248125afb
Zach P <zachary.perlman@juntacorp.com>
2019-11-05 17:57:40 -0500

    Updates

---
+++ precache-manifest.2eadc18bd7b469ea57739b30a0c1fa89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc97857539a2a4d69f2abcf1382237b",
+    "url": "/react-StarWars/index.html"
+  },
+  {
+    "revision": "1bbf3d142898a1cab154",
+    "url": "/react-StarWars/static/css/2.ed79d904.chunk.css"
+  },
+  {
+    "revision": "69295559698762eaad12",
+    "url": "/react-StarWars/static/css/main.d140e222.chunk.css"
+  },
+  {
+    "revision": "1bbf3d142898a1cab154",
+    "url": "/react-StarWars/static/js/2.9594c1b5.chunk.js"
+  },
+  {
+    "revision": "69295559698762eaad12",
+    "url": "/react-StarWars/static/js/main.32014ffb.chunk.js"
+  },
+  {
+    "revision": "2f0ec7499e622bb80ce4",
+    "url": "/react-StarWars/static/js/runtime-main.ef4fa8af.js"
+  },
+  {
+    "revision": "0a12a758def6aa207e9ddbc3cade937e",
+    "url": "/react-StarWars/static/media/logo.0a12a758.svg"
+  }
+]);
