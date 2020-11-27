d95ba6a3788e2907dcbe43d63abbb7a5badde3b4
zlerp <zhpdeals@gmail.com>
2020-11-27 16:32:13 -0500

    Updates

--- precache-manifest.6cad84ffde4bcfaa47073d0cc5efbaff.js
+++ precache-manifest.6cad84ffde4bcfaa47073d0cc5efbaff.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "167f3df36bc386392094c89dd26d5d6d",
+    "revision": "ca0e3fa2242ff0d69b3f514f9e77497e",
     "url": "/react-StarWars/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-StarWars/static/css/2.1697b39a.chunk.css"
   },
   {
-    "revision": "15cc0ae83bcbf45614e8",
+    "revision": "5fe1d49a671b75a66a96",
     "url": "/react-StarWars/static/css/main.b3055bfb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-StarWars/static/js/2.8a3351a7.chunk.js"
   },
   {
-    "revision": "15cc0ae83bcbf45614e8",
-    "url": "/react-StarWars/static/js/main.ba006033.chunk.js"
+    "revision": "5fe1d49a671b75a66a96",
+    "url": "/react-StarWars/static/js/main.28493a1f.chunk.js"
   },
   {
     "revision": "2f0ec7499e622bb80ce4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-StarWars/precache-manifest.79f1f6099127eaa7337a4dcd6bf497b8.js"
+  "/react-StarWars/precache-manifest.6cad84ffde4bcfaa47073d0cc5efbaff.js"
 );
 
 self.addEventListener('message', (event) => {
