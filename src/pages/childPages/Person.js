f87a0fecfefa37ca5e6a8229bf6f5bfceeb51866
zlerp <zhpdeals@gmail.com>
2020-11-27 18:33:13 -0500

    add proper people request url back

--- src/pages/childPages/Person.js
+++ src/pages/childPages/Person.js
@@ -34,7 +34,7 @@ class Person extends Component {
 
 
     componentDidMount() {
-        this.requestApi('https://swapi.dev/api/people/3');
+        this.requestApi(`${this.props.apiUrl}/people/${this.props.id}`.replace(/^http:\/\//i, 'https://'));
     }
 
 
